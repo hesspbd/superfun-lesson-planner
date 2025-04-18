e126586fa542863dca78c88020699db62e54520c
hesspbd <chen81428@gmail.com>
2025-04-18 11:31:24 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+document.getElementById("root").innerHTML = `
+  <h1 style='font-size:2rem;color:#333'>Super Fun 教案小幫手</h1>
+  <p>這是教案產生器的展示頁面。</p>
+  <ul>
+    <li>✔ 可選年級、主題</li>
+    <li>✔ 自動產生教學活動</li>
+    <li>✔ 適合 WordPress iframe 嵌入</li>
+  </ul>
+`;
